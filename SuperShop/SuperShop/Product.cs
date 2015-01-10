6290699817a0aced686f0581827cec6a80e75b69
RifatMizan <rifat.mizan@gmail.com>
2015-01-11 00:12:42 +0600

    updated

---
+++ SuperShop/SuperShop/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperShop
+{
+    class Product
+    {
+        public string ProductId { set; get; }
+        public int Quantity { set; get; }
+
+        public Product(string productId, int quantity)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+        }
+    }
+}
